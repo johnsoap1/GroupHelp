9cdeaf26bee88469cde7c532e631a50a9ae927f7
johnsoap1 <johnsoap1@proton.me>
2026-06-01 22:55:00 +0100

    oo

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -96,7 +96,8 @@ async def load_sudoers():
     sudoers = [] if not sudoers else sudoers.get("sudoers", [])
 
     for user_id in SUDO_USERS_ID:
-        SUDOERS.add(user_id)
+        if user_id not in SUDOERS:
+            SUDOERS.append(user_id)
 
         if user_id not in sudoers:
             sudoers.append(user_id)
@@ -107,7 +108,8 @@ async def load_sudoers():
             )
 
     for user_id in sudoers:
-        SUDOERS.add(user_id)
+        if user_id not in SUDOERS:
+            SUDOERS.append(user_id)
 
 
 # =========================
